61627e1c3456a3a293ce3cc72a3930c06f66cc4a
Kimsuyoon <129143104+Kimsuyoon@users.noreply.github.com>
2023-05-15 02:17:35 +0000

    조건문 마무리

---
+++ .ccls-cache/@home@runner@Ladybug/misson2.c
@@ -0,0 +1,20 @@
+// #include <stdio.h>
+// main() {
+//   int a;
+//   printf("당신의 시험 점수를 숫자로 입력해주세요. 당신의 학점을 알려드리겠습니다.");
+//   scanf("%d", &a);
+//   if (a>=100)
+//     printf("거짓말");
+//   else if(a>=90)
+//     printf("당신의 학점은 A학점입니다.");
+//   else if(a>=80)
+//     printf("당신의 학점은 B학점입니다.");
+//   else if(a>=70)
+//     printf("당신의 학점은 C학점입니다.");
+//   else if(a>=60)
+//     printf("당신의 학점은 D학점입니다.");
+//   else if(a>=0)
+//     printf("당신의 학점은 F학점입니다.");
+//   else
+//     printf("그런 점수는 세상에 없습니다.");
+// }
